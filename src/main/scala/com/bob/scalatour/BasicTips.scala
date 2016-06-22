3253c794cd8c985ff2ca4c48d10c3af27c0ec431
王相 <wangxiang@51zhangdan.com.cn>
2016-06-22 17:26:47 +0800

    for yield

--- src/main/scala/com/bob/scalatour/BasicTips.scala
+++ src/main/scala/com/bob/scalatour/BasicTips.scala
@@ -1,5 +1,13 @@
 package com.bob.scalatour
 
+trait Node
+
+case class TreeNode(v: String, left: Node, right: Node) extends Node
+
+case class Tree(root: TreeNode)
+
+case class User(uid: Int, name: String, age: Int, sex: Int)
+
 object BasicTips {
 
   /**
@@ -21,6 +29,82 @@ object BasicTips {
 
     val data = Seq.fill(10)(util.Random.nextString(6))
     echo(data: _*)
+
+    val tree = Tree(TreeNode("root", TreeNode("left", null, null), TreeNode("right", null, null)))
+    tree.root match {
+      case TreeNode(_, TreeNode("left", _, _), TreeNode("right", null, null)) => println("bingo")
+    }
+
+    //    findUser(19)(listUser)
+    //
+    //    findUser2(6)(listUser2)
+
+    testYieldInFor
+  }
+
+  def listUser(): List[User] = {
+    (0 to 10).map(x => {
+      User(x, s"name${x}", x, x % 2)
+    }).toList
+  }
+
+  def listUser2(name: String): List[User] = {
+    (0 to 10).map(x => {
+      User(x, s"name${x}", x, x % 2)
+    }).toList.filter(x => x.name == name)
+  }
+
+  def findUser2(uid: Int)(f: String => List[User]) = {
+    lazy val u = f("name1")
+    u.filter(x => x.uid == uid).foreach(println)
+    println("done")
+  }
+
+  /**
+   * 柯里化同时将函数做为一个入参
+   * @param uid
+   * @param f
+   * @return
+   */
+  def findUser(uid: Int)(f: () => List[User]) = {
+    lazy val users = f()
+    println("will invoke begin")
+    users.find(x => x.uid == uid).map(println)
+    println("end invoke")
+    users.find(x => x.uid == 2).map(println)
+  }
+
+  def testYieldInFor() = {
+    val afun = () => {
+      (1 to 100).map(x => {
+        x
+      })
+    }
+
+    val aafun = () => {
+      (101 to 200).map(x => {
+        x
+      })
+    }
+
+    import com.bob.scalatour.extens.TypeExtens._
+    time {
+      val ab = for {
+        file <- afun()
+        afile <- aafun()
+      } yield (file, afile)
+      ab.foreach(x => println(x))
+    }
+
+    time {
+      val ab = afun().flatMap(x => {
+        aafun().flatMap(y => {
+          List((x, y))
+        })
+      })
+      ab.foreach(x => println(x))
+    }
+
   }
 
 }
