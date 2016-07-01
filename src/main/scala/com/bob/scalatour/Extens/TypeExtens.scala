65015712389b379bce0eefb67eb8e9519f41c6c3
xiangwang <sevenz_da_best@hotmail.com>
2016-07-01 16:42:55 +0800

    add time function to calculate

--- src/main/scala/com/bob/scalatour/Extens/TypeExtens.scala
+++ src/main/scala/com/bob/scalatour/Extens/TypeExtens.scala
@@ -1,4 +1,4 @@
-package com.bob.scalatour.Extens
+package com.bob.scalatour.extens
 
 /**
  * 一系列类型扩展方法
@@ -30,6 +30,30 @@ object TypeExtens {
         case e: Exception => None
       }
     }
+  }
+
+  /**
+   * 模拟linux中shell管道
+   * @param t
+   * @tparam T
+   * @return
+   */
+  implicit def pipifunc[T](t: T) = new {
+    def |[R](f: T => R): R = f(t)
+  }
+
+  /**
+   * 传入一个函数，打印这个函数的执行时间
+   * @param code
+   * @tparam T
+   * @return
+   */
+  def time[T](code: => T) = {
+    val t0 = System.nanoTime: Double
+    val res = code
+    val t1 = System.nanoTime: Double
+    println("Elapsed time " + (t1 - t0) / 1000000.0 + " msecs")
+    res
   }
 
 }
